bd32f02a2b2686c2d030a247a66aaaa377ac4bc2
valeriasoars <valeriasoaressantos2@gmail.com>
2024-12-23 16:33:04 -0300

    Criando modelos

---
+++ aspnet-login-usuario/Models/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace aspnet_login_usuario.Models
+{
+    public class ResponseModel<T>
+    {
+        public T? Dados {  get; set; }
+        public string Mensagem { get; set; } = string.Empty;
+        public bool Status {  get; set; } = true;
+    }
+}
--- aspnet-login-usuario/obj/Release/net8.0/aspnet-login-usuario.AssemblyInfo.cs
+++ aspnet-login-usuario/obj/Release/net8.0/aspnet-login-usuario.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnet-login-usuario")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ae9038f461934934e1768973fdc64e62d812ec9")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnet-login-usuario")]
